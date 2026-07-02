279adf4a4bcd2e52b3d77a10e3edc1cc3dd7f578
ytworks <yuzo.takagi.private@gmail.com>
2026-07-02 11:44:29 +0900

    feat: add vLLM-compatible SamplingParams

--- kairyu/__init__.py
+++ kairyu/__init__.py
@@ -1,3 +1,7 @@
 """Kairyu: vLLM-compatible LLM inference framework with native orchestration."""
 
+from kairyu.sampling_params import SamplingParams
+
 __version__ = "0.1.0"
+
+__all__ = ["SamplingParams", "__version__"]
